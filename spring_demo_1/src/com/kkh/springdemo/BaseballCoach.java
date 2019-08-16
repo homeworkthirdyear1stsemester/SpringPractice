3e0fa533131dd95949013048646ce7dc305a5bd0
kihwankim <clack2933@gmail.com>
2019-08-16 23:21:31 +0900

    add main

---
+++ spring_demo_1/src/com/kkh/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.kkh.springdemo;
+
+public class BaseballCoach {
+
+    
+    public String getDailyWorkout() {
+        return "spend 30 minutes on bating practice";
+    }
+}
---
+++ spring_demo_1/src/com/kkh/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.kkh.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+        //create the object
+        BaseballCoach theCoach = new BaseballCoach();
+
+        //use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
