16db0131e5356c911612203585f8a6c34a878456
kihwankim <clack2933@gmail.com>
2019-08-17 20:44:01 +0900

    add load file and inject it

--- spring_demo_1/src/com/kkh/springdemo/CricketCoach.java
+++ spring_demo_1/src/com/kkh/springdemo/CricketCoach.java
@@ -4,9 +4,31 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // add new fileds for emailAddress and team
+    private String emailAddress;
+    private String team;
+
     // create a no-arg constructor
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return this.team;
     }
 
     public void setFortuneService(FortuneService fortuneService) {
