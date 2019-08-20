9aaefab38fbc7d8903de62aa9ae3da0b6546f285
kihwankim <clack2933@gmail.com>
2019-08-20 21:47:55 +0900

    check the default scope of bean

---
+++ spring_demo_1/src/com/kkh/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,25 @@
+package com.kkh.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+
+        // print out the results
+        System.out.println("\nPointing to the same object : " + result);
+        System.out.println("\nMemeory location for theCoach : " + theCoach);
+        System.out.println("\nMemeory location for alphaCoach : " + alphaCoach);
+
+        // close the context
+        context.close();
+    }
+}
