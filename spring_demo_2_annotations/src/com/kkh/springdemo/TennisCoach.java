86da2d1acc2f643d1391e40f26eb1b6fc1a59d6d
kihwankim <clack2933@gmail.com>
2019-08-21 21:03:32 +0900

    make injection by using autowired annotation

--- spring_demo_2_annotations/src/com/kkh/springdemo/TennisCoach.java
+++ spring_demo_2_annotations/src/com/kkh/springdemo/TennisCoach.java
@@ -1,12 +1,25 @@
 package com.kkh.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    @Autowired // fortuneService를 inject 한다
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return this.fortuneService.getFortune();
     }
 }
