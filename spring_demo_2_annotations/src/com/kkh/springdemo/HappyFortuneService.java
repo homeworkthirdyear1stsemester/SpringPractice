b51dc92d92c296882630c923f367974d41acabbf
kihwankim <clack2933@gmail.com>
2019-08-21 21:02:37 +0900

    add fortune class and interface

---
+++ spring_demo_2_annotations/src/com/kkh/springdemo/HappyFortuneService.java
@@ -0,0 +1,12 @@
+package com.kkh.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return "Today is youer lucky day!";
+    }
+}
