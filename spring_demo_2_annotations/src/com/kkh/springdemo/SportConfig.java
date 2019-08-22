910dd73e92675728c35dec64d73aed0984eebe28
kihwankim <clack2933@gmail.com>
2019-08-22 20:20:35 +0900

    make config java file and dont use xml

---
+++ spring_demo_2_annotations/src/com/kkh/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.kkh.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kkh.springdemo")
+public class SportConfig {
+
+}
