86428c6e1390e9c374c36f9604f7029dd7fd6e22
kihwankim <clack2933@gmail.com>
2019-09-04 18:19:51 +0900

    add annotation and class

---
+++ spring_demo_3_spring_mvc/src/com/kkh/springdemo/mvc/validation/CourseCodeConstrainValidator.java
@@ -0,0 +1,24 @@
+package com.kkh.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstrainValidator
+        implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode constraintAnnotation) {
+        this.coursePrefix = constraintAnnotation.value(); // annotation value
+    }
+
+    @Override
+    public boolean isValid(String theCode,
+                           ConstraintValidatorContext constraintValidatorContext) { // can place additional error messages
+
+        boolean result = theCode.startsWith(this.coursePrefix);
+
+        return result;
+    }
+}
