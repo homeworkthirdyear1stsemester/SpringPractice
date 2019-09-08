8c696b6dbc1530362566dfd4bff490f4922cf817
kihwankim <clack2933@gmail.com>
2019-09-08 19:20:08 +0900

    update db data

---
+++ spring_hibernate_tutorial/src/com/kkh/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,54 @@
+package com.kkh.hibernate.demo;
+
+import com.kkh.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateStudentDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            int studentId = 1;
+
+            // now get a new session and start transaction
+            session.beginTransaction();
+
+            // retrieve student based on the id : primary key
+            System.out.println("\nGetting student with id : " + studentId);
+            Student myStudent = session.get(Student.class, studentId); // db 접근해서 데이터 가져오기
+
+            System.out.println("Updating student...");
+            myStudent.setFirstName("stephe"); // db의 값을 update한다
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            /* NEW CODE */
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // update email for all students
+            System.out.println("Update email for all students");
+            session.createQuery("update Student set email='kkh@gmail.com'")
+                    .executeUpdate();
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!!!");
+        } finally {
+            factory.close();
+        }
+    }
+}
