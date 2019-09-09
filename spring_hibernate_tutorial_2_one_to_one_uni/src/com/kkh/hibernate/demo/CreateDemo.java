43bf7d5091cfa3aecc171e3bdac6abc93205b7e2
kihwankim <clack2933@gmail.com>
2019-09-09 21:17:55 +0900

    delete student class

--- spring_hibernate_tutorial_2_one_to_one_uni/src/com/kkh/hibernate/demo/CreateDemo.java
+++ spring_hibernate_tutorial_2_one_to_one_uni/src/com/kkh/hibernate/demo/CreateDemo.java
@@ -2,7 +2,6 @@ package com.kkh.hibernate.demo;
 
 import com.kkh.hibernate.demo.entity.Instructor;
 import com.kkh.hibernate.demo.entity.InstructorDetail;
-import com.kkh.hibernate.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
