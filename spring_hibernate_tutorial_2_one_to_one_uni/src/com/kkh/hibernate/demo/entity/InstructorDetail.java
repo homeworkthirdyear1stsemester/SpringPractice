1a10a5c6e89435b146ed6aa28c08b94ca769f7b9
kihwankim <clack2933@gmail.com>
2019-09-09 20:40:21 +0900

    add class for container and add it to db

---
+++ spring_hibernate_tutorial_2_one_to_one_uni/src/com/kkh/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,63 @@
+package com.kkh.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    // annotate the class as an entiry and map to db table
+
+    // define the fileds
+
+    // annotate the fileds with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String hobby;
+
+    @Column(name = "hobby")
+    private String youtubeChannel;
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String hobby, String youtubeChannel) {
+        this.hobby = hobby;
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHobby() {
+        return this.hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getYoutubeChannel() {
+        return this.youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+}
