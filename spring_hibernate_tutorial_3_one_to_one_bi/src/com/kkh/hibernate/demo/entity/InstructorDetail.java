2ee4ed3a8c2afa1f155a16fa2e751f9051cf7668
kihwankim <clack2933@gmail.com>
2019-09-09 21:58:48 +0900

    delete code and refactory all

--- spring_hibernate_tutorial_3_one_to_one_bi/src/com/kkh/hibernate/demo/entity/InstructorDetail.java
+++ spring_hibernate_tutorial_3_one_to_one_bi/src/com/kkh/hibernate/demo/entity/InstructorDetail.java
@@ -5,7 +5,13 @@ import javax.persistence.*;
 @Entity
 @Table(name = "instructor_detail")
 public class InstructorDetail {
-
+    /**
+     * CascadeType.RESIST – 엔티티를 생성하고, 연관 엔티티를 추가하였을 때 persist() 를 수행하면 연관 엔티티도 함께 persist()가 수행된다.  만약 연관 엔티티가 DB에 등록된 키값을 가지고 있다면 detached entity passed to persist Exception이 발생한다.
+     * CascadeType.MERGE – 트랜잭션이 종료되고 detach 상태에서 연관 엔티티를 추가하거나 변경된 이후에 부모 엔티티가 merge()를 수행하게 되면 변경사항이 적용된다.(연관 엔티티의 추가 및 수정 모두 반영됨)
+     * CascadeType.REMOVE – 삭제 시 연관된 엔티티도 같이 삭제됨
+     * CascadeType.DETACH – 부모 엔티티가 detach()를 수행하게 되면, 연관된 엔티티도 detach() 상태가 되어 변경사항이 반영되지 않는다.
+     * CascadeType.ALL – 모든 Cascade 적용
+     */
     // annotate the class as an entiry and map to db table
 
     // define the fileds
@@ -32,7 +38,7 @@ public class InstructorDetail {
     // add new field for instructor (also add getter/setters)
     // add @OneToOne annotation
     // instructor객체가 가지고있는 instructorDetail이라는 변수명을 가진 변수와 연결
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private Instructor instructor;
 
     public InstructorDetail() {
