a2f35e920aafc11f366d2b7072db9b6b0b7d973c
kihwankim <clack2933@gmail.com>
2019-09-12 16:10:51 +0900

    add only for bi connection to db

---
+++ spring_hibernate_tutorial_4_one_to_many/src/com/kkh/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,59 @@
+package com.kkh.hibernate.demo;
+
+import com.kkh.hibernate.demo.entity.Course;
+import com.kkh.hibernate.demo.entity.Instructor;
+import com.kkh.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class CreateInstructorDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // create the objects
+            Instructor instructor = new Instructor("kh", "kim", "hello@kkh.com");
+
+            InstructorDetail instructorDetail = new InstructorDetail(
+                    "http://www.kkh.com/youtube",
+                    "hello for code"
+            );
+
+            // associate the objects
+            instructor.setInstructorDetail(instructorDetail);
+
+            // start a transaction 거래 시작
+            session.beginTransaction();
+
+            /*
+             save the instructor
+             Note : this will ALSO save the details object
+             because CascadeType.ALL
+             */
+            System.out.println("Saving instructor : " + instructor);
+            session.save(instructor);
+
+            // commit transaction 거래 성사
+            session.getTransaction().commit();
+
+            System.out.println("Done!!!");
+        } finally {
+            // add clean up code
+            session.close();
+            factory.close();
+        }
+    }
+}
