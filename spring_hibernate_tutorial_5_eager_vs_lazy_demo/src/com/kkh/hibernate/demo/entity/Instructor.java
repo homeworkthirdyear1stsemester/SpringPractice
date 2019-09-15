1e1053920229f6b250f7cce8cadb3048e0544c6f
kihwankim <clack2933@gmail.com>
2019-09-15 11:21:37 +0900

    fetch Lazy test by debugging

--- spring_hibernate_tutorial_5_eager_vs_lazy_demo/src/com/kkh/hibernate/demo/entity/Instructor.java
+++ spring_hibernate_tutorial_5_eager_vs_lazy_demo/src/com/kkh/hibernate/demo/entity/Instructor.java
@@ -25,16 +25,21 @@ public class Instructor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
+
     @Column(name = "email")
     private String email;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
-    @OneToMany(fetch = FetchType.EAGER, // default is Lazy
+
+    @OneToMany(fetch = FetchType.LAZY, // default is Lazy
             mappedBy = "instructor",
             cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                     CascadeType.DETACH, CascadeType.REFRESH})
