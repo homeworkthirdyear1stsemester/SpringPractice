a93035d5a5c6e4e1e7b9e1e1a0c79fbdc3d84f06
kihwankim <clack2933@gmail.com>
2019-09-15 13:27:06 +0900

    get course and review data from db

---
+++ spring_hibernate_tutorial_6_one_to_many_uni/src/com/kkh/hibernate/demo/GetCourseAndReviewsDemo.java
@@ -0,0 +1,52 @@
+package com.kkh.hibernate.demo;
+
+import com.kkh.hibernate.demo.entity.Course;
+import com.kkh.hibernate.demo.entity.Instructor;
+import com.kkh.hibernate.demo.entity.InstructorDetail;
+import com.kkh.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class GetCourseAndReviewsDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction 거래 시작
+            session.beginTransaction();
+
+            // get the course
+            int theId = 10;
+            Course course = session.get(Course.class, theId);
+
+            // print the course
+            System.out.println(course);
+
+            // print the course reviews
+            System.out.println(course.getReviews());
+
+            // commit transaction 거래 성사
+            session.getTransaction().commit();
+
+            System.out.println("Done!!!");
+        } finally {
+            // add clean up code
+            session.close();
+            factory.close();
+        }
+    }
+}
