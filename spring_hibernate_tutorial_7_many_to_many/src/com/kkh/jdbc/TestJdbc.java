4e1463780fd396221171c048129e69beed1f562e
kihwankim <clack2933@gmail.com>
2019-09-15 16:08:03 +0900

    init

---
+++ spring_hibernate_tutorial_7_many_to_many/src/com/kkh/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package com.kkh.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-05-many-to-many?useSSL=false"; // userSSL=false 는 warning 없이 한다
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database : " + jdbcUrl);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+            System.out.println("Connection successful!!!");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
